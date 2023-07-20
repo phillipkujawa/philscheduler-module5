433a42ef8dfaee13db2ee32508bc3b103ad67f47
phillipkujawa <phillip.kujawa@me.com>
2023-07-20 06:28:37 -0400

    added code to support saving schdule content to localstorage

--- app.js
+++ app.js
@@ -4,29 +4,37 @@ window.onload = function() {
     const currentDate = dayjs();
     currentDayElement.innerText = currentDate.format('MMMM D, YYYY');
 
+    $('.saveBtn').on('click', function () {
+        // get nearby values
+        var userDesc = $(this).siblings('.description').val();
+        var time = $(this).parent().attr('id');
+    
+    localStorage.setItem(time, userDesc);
+    
+    });
     // Create time blocks
-    for (let hour = 9; hour <= 23; hour++) {
-        const timeBlockElement = document.createElement('div');
-        timeBlockElement.id = `hour-${hour}`;
-        timeBlockElement.innerText = `${hour}:00`;
+    // for (let hour = 9; hour <= 23; hour++) {
+    //     const timeBlockElement = document.createElement('div');
+    //     timeBlockElement.id = `hour-${hour}`;
+    //     timeBlockElement.innerText = `${hour}:00`;
 
-        // Color code the time block
-        const currentHour = currentDate.hour();
-        if (hour < currentHour) {
-            timeBlockElement.className = 'past';
-        } else if (hour === currentHour) {
-            timeBlockElement.className = 'present';
-        } else {
-            timeBlockElement.className = 'future';
-        }
+    //     // Color code the time block
+    //     const currentHour = currentDate.hour();
+    //     if (hour < currentHour) {
+    //         timeBlockElement.className = 'past';
+    //     } else if (hour === currentHour) {
+    //         timeBlockElement.className = 'present';
+    //     } else {
+    //         timeBlockElement.className = 'future';
+    //     }
 
-        timeBlockElement.onclick = function() {
-            const event = prompt('Enter an event:');
-            if (event) {
-                localStorage.setItem(`event-${hour}`, event);
-                timeBlockElement.innerText = `${hour}:00 - ${event}`;
-            }
-        }
+    //     timeBlockElement.onclick = function() {
+    //         const event = prompt('Enter an event:');
+    //         if (event) {
+    //             localStorage.setItem(`event-${hour}`, event);
+    //             timeBlockElement.innerText = `${hour}:00 - ${event}`;
+    //         }
+    //     }
 
         // Load saved event
         const savedEvent = localStorage.getItem(`event-${hour}`);
@@ -36,4 +44,4 @@ window.onload = function() {
 
         timeBlocksElement.appendChild(timeBlockElement);
     }
-}
+
